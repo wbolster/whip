7bff3aab8b011495d109627ce58df3bdc17cde86
Wouter Bolsterlee <uws@xs4all.nl>
2013-09-03 13:21:46 +0200

    Expand Quova conversion script

--- convert-quova.py
+++ convert-quova.py
@@ -6,9 +6,13 @@ import argparse
 import csv
 import gzip
 import itertools
+import logging
+import os
+import re
 import signal
 import sys
 
+logger = logging.getLogger(__name__)
 
 REF_HEADERS = frozenset(['carrier', 'org', 'sld', 'tld'])
 
@@ -116,18 +120,18 @@ def convert_to_v7(data_fp, ref_fp, out_fp):
         'sld': {},
         'tld': {},
     }
-    with ref_fp:
-        ref_reader = csv.reader(ref_fp, delimiter='|')
+    ref_reader = csv.reader(ref_fp, delimiter='|')
 
-        for row in ref_reader:
-            if not row[0] in REF_HEADERS:
-                raise ValueError(
-                    "Unexpected input in reference data file: expected "
-                    "header line, got %r" % (row))
+    logger.info("Reading reference file %r...", ref_fp.name)
+    for row in ref_reader:
+        if not row[0] in REF_HEADERS:
+            raise ValueError(
+                "Unexpected input in reference data file: expected "
+                "header line, got %r" % (row))
 
-            ref_type, n_records, max_id = row
-            for ref_id, value in itertools.islice(ref_reader, int(n_records)):
-                refs[ref_type][int(ref_id)] = clean(value)
+        ref_type, n_records, max_id = row
+        for ref_id, value in itertools.islice(ref_reader, int(n_records)):
+            refs[ref_type][int(ref_id)] = clean(value)
 
     # Prepare reader
     reader = csv.DictReader(data_fp, IN_FIELDS, delimiter='|')
@@ -144,7 +148,8 @@ def convert_to_v7(data_fp, ref_fp, out_fp):
     writer.writeheader()
 
     # Loop and transform
-    for record in reader:
+    logger.info("Transforming data file %r...", data_fp.name)
+    for n, record in enumerate(reader, 1):
         for k, v in record.iteritems():
             record[k] = clean(v)
 
@@ -170,21 +175,54 @@ def convert_to_v7(data_fp, ref_fp, out_fp):
         # Write output
         writer.writerow(record)
 
+        if n % 100000 == 0:
+            logger.info("Converted %d input records", n)
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Convert an older Quova data set into V7 format")
-    parser.add_argument('data', type=gzip.open)
-    parser.add_argument('reference', type=gzip.open)
-    parser.add_argument('--output', '-o', type=argparse.FileType('w'),
-                        nargs='?', default=sys.stdout)
+    parser.add_argument('data_file')
+    parser.add_argument('--output-directory', '-o', required=True)
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
 
     # Don't raise IOError messages on broken pipes, e.g. when using
     # "... | head"
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
-    convert_to_v7(args.data, args.reference, args.output)
+    filename_regex = re.compile(
+        r'''
+        ^
+        EDITION_Gold_
+        (?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})
+        _v(?P<version>.+)
+        \.dat\.gz
+        $
+        ''',
+        re.VERBOSE)
+    m = re.match(filename_regex, os.path.basename(args.data_file))
+    if not m:
+        logger.error(
+            "Input file name %r does not match expected file name format",
+            args.data_file)
+        sys.exit(1)
+
+    fmt = 'converted_v{version}_dummy_{year}{month}{day}.csv.gz'
+    output_filename = fmt.format(**m.groupdict())
+
+    output_path = os.path.join(args.output_directory, output_filename)
+    reference_file = args.data_file.replace('.dat', '.ref')
+
+    logger.info(
+        "Converting input files %r and %r to output file %r",
+        args.data_file, reference_file, output_path)
+
+    data_fp = gzip.open(args.data_file, 'r')
+    reference_fp = gzip.open(reference_file, 'r')
+    output_fp = gzip.open(output_path, 'w')
+    convert_to_v7(data_fp, reference_fp, output_fp)
 
 
 if __name__ == '__main__':
