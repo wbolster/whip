fd88f20fd8fd6affc2f5a242fb042c16e1b0f942
Wouter Bolsterlee <uws@xs4all.nl>
2013-09-03 13:40:39 +0200

    Merge Quova conversion script into main package
    Moved the conversion routines to the importer subpackage, and add a
    whip-cli subcommand from the original main() routine.

--- whip/importers/quova.py
+++ whip/importers/quova.py
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+
 """
 Importer for Quova data sets.
 """
@@ -51,7 +52,7 @@ DATA_FILE_RE = re.compile(r'''
 NUMERIC_FIELDS = frozenset(('as', 'country_cf', 'state_cf', 'city_cf'))
 
 # Description of all fields in the .dat files
-QuovaRecord = collections.namedtuple('QuovaRecord', (
+FIELDS = (
     'start_ip_int',
     'end_ip_int',
     'continent',
@@ -80,7 +81,10 @@ QuovaRecord = collections.namedtuple('QuovaRecord', (
     'organization',
     'carrier',
     'anonymizer_status',
-))
+)
+
+# Structure for input records
+QuovaRecord = collections.namedtuple('QuovaRecord', FIELDS)
 
 
 def clean(v):
@@ -91,6 +95,11 @@ def clean(v):
 
     return v
 
+
+
+#
+# Importer
+#
 
 class QuovaImporter(object):
     """Importer for Quova data sets."""
@@ -190,3 +199,143 @@ class QuovaImporter(object):
 
         reporter.tick(True)
         logger.info("Finished reading %r (%d records)", data_file, n)
+
+
+#
+# Old data format conversion
+#
+
+OLD_FORMAT_REF_HEADERS = frozenset(['carrier', 'org', 'sld', 'tld'])
+
+OLD_FORMAT_FIELDS = (
+    'start_ip_int',
+    'end_ip_int',
+    'cidr',
+    'continent',
+    'country',
+    'country_iso2',
+    'country_cf',
+    'region',
+    'state',
+    'state_cf',
+    'city',
+    'city_cf',
+    'postal_code',
+    'phone_number_prefix',
+    'timezone',
+    'latitude',
+    'longitude',
+    'dma',
+    'msa',
+    'pmsa',
+    'connectiontype',
+    'linespeed',
+    'ip_routingtype',
+    'aol',
+    'asn',
+    'sld_id',
+    'tld_id',
+    'reg_org_id',
+    'carrier_id',
+)
+
+OLD_FORMAT_NUMERICAL_FIELDS = [
+    'asn',
+    'city_cf',
+    'country_cf',
+    'dma',
+    'msa',
+    'phone_number_prefix',
+    'postal_code',
+    'state_cf',
+]
+
+OLD_FORMAT_RENAMED_FIELDS = {
+    'connectiontype': 'connection_type',
+    'country_iso2': 'country_code',
+    'ip_routingtype': 'ip_routing_type',
+    'linespeed': 'line_speed',
+    'phone_number_prefix': 'area_code',
+    'timezone': 'time_zone',
+}
+
+OLD_FORMAT_EMPTY_VALUES = frozenset(('', 'unknown', 'none'))
+
+
+def clean_old_format(v):
+    """Clean an input field (old data format)"""
+    if v in OLD_FORMAT_EMPTY_VALUES:
+        return None
+
+    return v
+
+
+def convert_to_v7(data_fp, ref_fp, out_fp):
+    """Convert old format data and reference files to the V7 format"""
+
+    # TODO: optionally include anonymous proxies data
+
+    # Reference data (lookups)
+    refs = {
+        'carrier': {},
+        'org': {},
+        'sld': {},
+        'tld': {},
+    }
+    ref_reader = csv.reader(ref_fp, delimiter='|')
+
+    logger.info("Reading reference file %r", ref_fp.name)
+    for row in ref_reader:
+        if not row[0] in OLD_FORMAT_REF_HEADERS:
+            raise ValueError(
+                "Unexpected input in reference data file: expected "
+                "header line, got %r" % (row))
+
+        ref_type, n_records, max_id = row
+        for ref_id, value in itertools.islice(ref_reader, int(n_records)):
+            refs[ref_type][int(ref_id)] = clean_old_format(value)
+
+    # Prepare reader
+    reader = csv.DictReader(data_fp, OLD_FORMAT_FIELDS, delimiter='|')
+
+    # Prepare writer
+    writer = csv.DictWriter(
+        out_fp, FIELDS,
+        delimiter=',',
+        quoting=csv.QUOTE_ALL,
+        quotechar='"',
+        lineterminator='\n',
+        extrasaction='ignore',
+    )
+    writer.writeheader()
+
+    # Loop and transform
+    logger.info("Transforming data file %r", data_fp.name)
+    for n, record in enumerate(reader, 1):
+        for k, v in record.iteritems():
+            record[k] = clean_old_format(v)
+
+        # Replace missing numerical values by empty strings
+        for key in OLD_FORMAT_NUMERICAL_FIELDS:
+            if record[key] == '0':
+                record[key] = ''
+
+        # Rename some fields
+        for k1, k2 in OLD_FORMAT_RENAMED_FIELDS.iteritems():
+            record[k2] = record.pop(k1)
+
+        # Reference lookups
+        record['carrier'] = refs['carrier'][int(record['carrier_id'])]
+        record['organization'] = refs['org'][int(record['reg_org_id'])]
+        record['sld'] = refs['sld'][int(record['sld_id'])]
+        record['tld'] = refs['tld'][int(record['tld_id'])]
+
+        # Drop magic "empty" value for time zones
+        if record['time_zone'] == '999':
+            record['time_zone'] = ''
+
+        # Write output
+        writer.writerow(record)
+
+        if n % 100000 == 0:
+            logger.info("Converted %d input records", n)
