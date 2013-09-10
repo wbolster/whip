827cf4e82a192d8d2db1d70d7c48f69aa1823a61
Wouter Bolsterlee <uws@xs4all.nl>
2013-09-10 16:10:47 +0200

    Add whip-cli console entry point to setup.py script

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-
 from setuptools import find_packages, setup
-
 
 setup(
     name='Whip',
     version='0.1',
     packages=find_packages(exclude=['tests']),
+    entry_points={
+        'console_scripts': [
+            'whip-cli = whip.cli:main',
+        ],
+    }
 )
