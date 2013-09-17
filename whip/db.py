4a4661b4bc03e0d3116fc77246841d32e2e8aecd
Wouter Bolsterlee <uws@xs4all.nl>
2013-09-17 10:35:01 +0200

    Refactor history squashing code into helper function

--- whip/db.py
+++ whip/db.py
@@ -56,7 +56,52 @@ def _debug_format_infoset(d):
                      for k, v in sorted(d.iteritems()))
 
 
-def _build_db_record(begin_ip_int, end_ip_int, infosets, _ig1=itemgetter(1)):
+def _squash_infosets(infosets, _ig1=itemgetter(1)):
+    """Squash history by grouping adjacent identical infosets.
+
+    This functions takes a list of infosets and returns only the unique
+    information, sorted chronologically. Adjacent infosets with
+    identical information (i.e. unchanged information) will be merged
+    into a single infoset using the oldest timestamp (i.e. when did this
+    information first occur).
+    """
+
+    # Step 1: preparation. Each infoset will have a different timestamp,
+    # so first pop (and remember) the timestamp from each infoset before
+    # doing the actual comparison, then sort by ascending date so that
+    # we can iterate chronologically.
+    dates_and_info = [(d.pop('datetime'), d) for d in infosets]
+    dates_and_info.sort()
+
+    # Step 2: deduplication. Group identical information, not taking the
+    # date into account, and only keep the oldest occurrence of each
+    # unique infoset.
+    #
+    # Implementation notes:
+    #
+    # * Approach is based on the unique_justseen() recipe from the
+    #   itertools docs.
+    #
+    # * The infoset in each (dt, infoset) tuple serves as the key for
+    #   the grouper, and the imap(next, imap(...)) trick extracts the
+    #   first infoset from each group.
+    #
+    # * Turn the result into a list, since the code below modifies the
+    #   dicts, which would break the grouping if done lazily, since the
+    #   grouping compares the dicts.
+    squashed = list(imap(next, imap(_ig1, groupby(dates_and_info, _ig1))))
+
+    # Step 3: transform to original format. Add back the timestamp to
+    # obtain infosets in the original format.
+    result = []
+    for dt, infoset in squashed:
+        infoset['datetime'] = dt  # store oldest datetime for this infoset
+        result.append(infoset)
+
+    return result
+
+
+def _build_db_record(begin_ip_int, end_ip_int, infosets):
     """Create database records for an iterable of merged infosets."""
 
     assert len(infosets) > 0
@@ -65,35 +110,8 @@ def _build_db_record(begin_ip_int, end_ip_int, infosets, _ig1=itemgetter(1)):
     # we can mutate them.
     infosets = [x.copy() for x in infosets]
 
-    # Squash history by grouping adjacent identical infosets. Each
-    # infoset will have a different timestamp, so first pop (and
-    # remember) the timestamp from each infoset before doing the actual
-    # comparison, then sort by ascending date so that we can iterate
-    # chronologically.
-    dates_and_info = [(d.pop('datetime'), d) for d in infosets]
-    dates_and_info.sort()
-
-    # Now group identical information (not taking the date into
-    # account), only keeping its oldest occurrence. This deduplication
-    # approach is based on the unique_justseen() recipe from the
-    # itertools docs. Notes:
-    #
-    # * The infoset in each (dt, infoset) tuple serves as the key for
-    #   the grouper, and the imap(next, imap(...)) trick extracts the
-    #   first infoset from each group.
-    #
-    # * Turn the result into a list, since subsequent code modifies the
-    #   dicts, which would break the grouping if done lazily, since the
-    #   grouping compares the dicts.
-    squashed = list(imap(next, imap(_ig1, groupby(dates_and_info, _ig1))))
-
-    # Add back the first timestamp to obtain infosets in the original
-    # format. After this loop 'unique_infosets' contains only unique
-    # information.
-    unique_infosets = []
-    for dt, infoset in squashed:
-        infoset['datetime'] = dt  # store oldest datetime for this infoset
-        unique_infosets.append(infoset)
+    # Deduplicate
+    unique_infosets = _squash_infosets(infosets)
 
     # The most recent infoset is stored in full
     latest = unique_infosets[-1]
