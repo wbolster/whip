10d0caa388b1d72e4539995c2de6c3e7696e08f2
Wouter Bolsterlee <uws@xs4all.nl>
2013-10-15 16:36:49 +0200

    Expand tests to cover various loading scenarios

--- tests/test_db.py
+++ tests/test_db.py
@@ -47,40 +47,64 @@ def test_db_loading():
         ],
     ]
 
-    def check_sanity(db):
+    s1, s2, s3 = snapshots
 
-        # Latest version
-        assert lookup(db, '1.0.0.0')['x'] == 4
-        assert lookup(db, '1.255.255.255')['x'] == 4
-        assert lookup(db, '7.0.0.0') == {}
-        assert lookup(db, '8.1.2.3')['x'] == 6
-        assert lookup(db, '12.0.0.0') == {}
+    def check_sanity(*snapshots_lists):
+        with tempfile.TemporaryDirectory() as db_dir:
+            db = Database(db_dir, create_if_missing=True)
 
-        # Specific dates
-        assert lookup(db, '1.2.3.3', '2010')['x'] == 1
-        assert lookup(db, '1.2.3.4', '2011')['x'] == 7
-        assert lookup(db, '1.2.3.5', '2011')['x'] == 8
-        assert lookup(db, '1.100.100.100', '2011')['x'] == 1
-        assert lookup(db, '8.1.2.3', '2011')['x'] == 3
+            for snapshots in snapshots_lists:
+                iters = [iter_snapshot(s) for s in snapshots]
+                db.load(*iters)
 
-        # No hit for really old dates
-        assert lookup(db, '1.2.3.4', '2009') == {}
+            # Latest version
+            assert lookup(db, '1.0.0.0')['x'] == 4
+            assert lookup(db, '1.255.255.255')['x'] == 4
+            assert lookup(db, '7.0.0.0') == {}
+            assert lookup(db, '8.1.2.3')['x'] == 6
+            assert lookup(db, '12.0.0.0') == {}
 
-        # Future date
-        assert lookup(db, '1.2.3.4', '2038')['x'] == 4
+            # Specific dates
+            assert lookup(db, '1.2.3.3', '2010')['x'] == 1
+            assert lookup(db, '1.2.3.4', '2011')['x'] == 7
+            assert lookup(db, '1.2.3.5', '2011')['x'] == 8
+            assert lookup(db, '1.100.100.100', '2011')['x'] == 1
+            assert lookup(db, '8.1.2.3', '2011')['x'] == 3
 
-        # All versions
-        assert lookup_all_x(db, '1.2.3.4') == [4, 7, 1]
+            # No hit for really old dates
+            assert lookup(db, '1.2.3.4', '2009') == {}
 
-    with tempfile.TemporaryDirectory() as db_dir:
-        db = Database(db_dir, create_if_missing=True)
-        iters = [iter_snapshot(s) for s in snapshots]
+            # Future date
+            assert lookup(db, '1.2.3.4', '2038')['x'] == 4
 
-        # db.load(*iters)
+            # All versions
+            assert lookup_all_x(db, '1.2.3.4') == [4, 7, 1]
 
-        db.load(*iters[:-1])
-        db.load(iters[-1])
-        db.load(iters[-1])
-        db.load()
+    # Test loading in various combinations and check whether the
+    # resulting database is correct.
 
-        check_sanity(db)
+    # All at once
+    check_sanity(
+        [s1, s2, s3]
+    )
+
+    # One at a time
+    check_sanity(
+        [s1],
+        [s2],
+        [s3],
+    )
+
+    # In chunks
+    check_sanity(
+        [s1, s2],
+        [s3],
+        [],
+    )
+
+    # Randomized with duplicate snapshots
+    check_sanity(
+        [s3, s2],
+        [s2, s1],
+        [s1],
+    )
