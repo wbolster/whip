6e95130d762708f2d9a78c29da9a476fd6f1a82a
Wouter Bolsterlee <uws@xs4all.nl>
2013-10-21 17:00:19 +0200

    Handle IPv6 in input reader

--- whip/reader.py
+++ whip/reader.py
@@ -3,7 +3,7 @@ Whip reader module.
 """
 
 from .json import loads
-from .util import ipv4_str_to_int
+from .util import ip_str_to_int
 
 DEFAULT_RANGE_FIELDS = ('begin', 'end')
 
@@ -12,8 +12,8 @@ def iter_json(fp, range_fields=DEFAULT_RANGE_FIELDS):
     """Read a JSON formatted stream of data from a file like object.
 
     Each line in the input file must contain a valid JSON document. The
-    fields specified by `range_fields` should contain IPv4 addresses
-    that are used as the beginning and end of the range.
+    fields specified by `range_fields` should contain IP addresses that
+    are used as the beginning and end of the range.
 
     The input must already be sorted by IP range, and the ranges must
     not overlap.
@@ -24,13 +24,13 @@ def iter_json(fp, range_fields=DEFAULT_RANGE_FIELDS):
     """
 
     begin_field, end_field = range_fields
-    _ipv4_str_to_int = ipv4_str_to_int
+    _ip_str_to_int = ip_str_to_int
     _loads = loads
 
     for line in fp:
         doc = _loads(line)
         yield (
-            _ipv4_str_to_int(doc[begin_field]),
-            _ipv4_str_to_int(doc[end_field]),
+            _ip_str_to_int(doc[begin_field]),
+            _ip_str_to_int(doc[end_field]),
             doc,
         )
