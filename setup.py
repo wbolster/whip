1e552d2bfd953055e1b781dba2e058bb88a3b1a4
Wouter Bolsterlee <uws@xs4all.nl>
2015-01-24 17:19:03 +0100

    Add a lot more metadata to setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='whip',
     version='0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url='https://github.com/wbolster/whip',
+    license='BSD',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'aaargh',
@@ -15,5 +19,12 @@ setup(
         'console_scripts': [
             'whip-cli = whip.cli:main',
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+    ],
 )
