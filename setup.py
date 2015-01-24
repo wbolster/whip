4545ff29a5f3e3b9f2ea41a738e3edca46a18ad3
Wouter Bolsterlee <uws@xs4all.nl>
2015-01-24 17:17:30 +0100

    Add install_requires to setup script

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ setup(
     name='whip',
     version='0.1',
     packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'aaargh',
+        'Flask',
+        'msgpack-python',
+        'plyvel',
+        'ujson',
+    ],
     entry_points={
         'console_scripts': [
             'whip-cli = whip.cli:main',
